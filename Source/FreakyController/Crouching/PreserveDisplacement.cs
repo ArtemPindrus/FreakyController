df89db2f2f6392c85a7e0c86bfb66f104351f99a
Artem Pindrus <artempindrus@gmail.com>
2025-06-20 11:09:18 +0300

    Create PreserveDisplacement.cs

---
+++ Source/FreakyController/Crouching/PreserveDisplacement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace FreakyController;
+
+/// <summary>
+/// PreserveDisplacement Script.
+/// </summary>
+public class PreserveDisplacement : Script
+{
+    [Serialize]
+    [ShowInEditor]
+    private Controller controller;
+
+    private float initialHeight;
+    private Vector3 initialLocalDisplacement;
+
+    public override void OnAwake()
+    {
+        initialLocalDisplacement = Actor.LocalPosition;
+        initialHeight = controller.Height;
+    }
+
+    public override void OnUpdate() {
+        Actor.LocalPosition = controller.Height * initialLocalDisplacement / initialHeight;
+    }
+}
